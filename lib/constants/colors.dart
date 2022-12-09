0e6275bc99c0af6ce24ef5ec176caaeea718e9b5
Dilzo16 <chathuradilshanudawaththa@gmail.com>
2022-12-09 20:23:38 +0530

    UI-Changed

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MainColor {
-  static Color primaryColor = const Color(0xFFff4b4b);
+  static Color primaryColor = Color.fromARGB(255, 3, 3, 0);
   static Color secondaryColor = const Color(0xFFffca27);
-  static Color asccentColor = const Color(0xFF4169e8);
+  static Color asccentColor = Color.fromARGB(255, 234, 4, 255);
 }
